cb92eef825d28118fc993ef91199325243bd4326
Ky Bui Van <ky.buivan.dev@gmail.com>
2023-02-23 12:59:23 +0700

    Update imgui fail in workflow build

--- external/imgui.cmake
+++ external/imgui.cmake
@@ -1,34 +1,34 @@
-include(glfw)
-
 FetchContent_Declare(
     imgui
     GIT_REPOSITORY https://github.com/ocornut/imgui.git
-    GIT_TAG fad8afd62ad1cd7051f1e5dd2f7a88cb42f6ab88 # docking
+    GIT_TAG docking
     GIT_SHALLOW TRUE
 )
 
-FetchContent_MakeAvailable(imgui)
+FetchContent_GetProperties(imgui)
+if(NOT imgui_POPULATED)
+    FetchContent_Populate(imgui)
+    add_library(imgui STATIC
+        "${imgui_SOURCE_DIR}/imconfig.h"
+        "${imgui_SOURCE_DIR}/imgui.h"
+        "${imgui_SOURCE_DIR}/imgui.cpp"
+        "${imgui_SOURCE_DIR}/imgui_demo.cpp"
+        "${imgui_SOURCE_DIR}/imgui_draw.cpp"
+        "${imgui_SOURCE_DIR}/imgui_internal.h"
+        "${imgui_SOURCE_DIR}/imgui_tables.cpp"
+        "${imgui_SOURCE_DIR}/imgui_widgets.cpp"
+        "${imgui_SOURCE_DIR}/imstb_rectpack.h"
+        "${imgui_SOURCE_DIR}/imstb_textedit.h"
+        "${imgui_SOURCE_DIR}/imstb_truetype.h"
+        "${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.h"
+        "${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp"
+        "${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h"
+        "${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp"
+    )
 
-add_library(imgui STATIC
-    "${imgui_SOURCE_DIR}/imconfig.h"
-    "${imgui_SOURCE_DIR}/imgui.h"
-    "${imgui_SOURCE_DIR}/imgui.cpp"
-    "${imgui_SOURCE_DIR}/imgui_demo.cpp"
-    "${imgui_SOURCE_DIR}/imgui_draw.cpp"
-    "${imgui_SOURCE_DIR}/imgui_internal.h"
-    "${imgui_SOURCE_DIR}/imgui_tables.cpp"
-    "${imgui_SOURCE_DIR}/imgui_widgets.cpp"
-    "${imgui_SOURCE_DIR}/imstb_rectpack.h"
-    "${imgui_SOURCE_DIR}/imstb_textedit.h"
-    "${imgui_SOURCE_DIR}/imstb_truetype.h"
-    "${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.h"
-    "${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp"
-    "${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h"
-    "${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp"
-)
+    find_package(OpenGL REQUIRED)
 
-find_package(OpenGL REQUIRED)
-
-target_link_libraries(imgui PUBLIC glfw OpenGL::GL)
-target_include_directories(imgui PUBLIC "${imgui_SOURCE_DIR}" "${imgui_SOURCE_DIR}/backends")
-add_library(imgui::imgui ALIAS imgui)
+    target_link_libraries(imgui PUBLIC glfw OpenGL::GL)
+    target_include_directories(imgui PUBLIC "${imgui_SOURCE_DIR}" "${imgui_SOURCE_DIR}/backends")
+    add_library(imgui::imgui ALIAS imgui)
+endif()
