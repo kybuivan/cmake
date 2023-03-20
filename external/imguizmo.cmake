372a5c552821b6e2f5babd64aca8e671362539c1
Ky Bui Van <ky.buivan.dev@gmail.com>
2023-03-20 10:50:56 +0700

    Update imnodes and imguizmo define "IMGUI_DEFINE_MATH_OPERATORS"

--- external/imguizmo.cmake
+++ external/imguizmo.cmake
@@ -1,14 +1,3 @@
-#
-# Copyright 2020 Adobe. All rights reserved.
-# This file is licensed to you under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License. You may obtain a copy
-# of the License at http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under
-# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-# OF ANY KIND, either express or implied. See the License for the specific language
-# governing permissions and limitations under the License.
-#
 if(TARGET imguizmo::imguizmo)
     return()
 endif()
@@ -19,7 +8,7 @@ include(FetchContent)
 FetchContent_Declare(
     imguizmo
     GIT_REPOSITORY https://github.com/CedricGuillemet/ImGuizmo.git
-    GIT_TAG ace44b6ddae5c9ee7e48d8e25589838ec40227fa
+    GIT_TAG master
 )
 FetchContent_MakeAvailable(imguizmo)
 
@@ -30,11 +19,8 @@ add_library(imguizmo
 add_library(imguizmo::imguizmo ALIAS imguizmo)
 
 target_include_directories(imguizmo PUBLIC "${imguizmo_SOURCE_DIR}")
-
-include(imgui)
 target_link_libraries(imguizmo PUBLIC imgui::imgui)
-
-set_target_properties(imguizmo PROPERTIES FOLDER third_party)
+target_compile_definitions(imguizmo PUBLIC IMGUI_DEFINE_MATH_OPERATORS)
 
 if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
     target_compile_options(imguizmo PRIVATE
