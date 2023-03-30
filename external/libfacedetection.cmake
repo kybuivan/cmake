322d5bd77be7bf85b6d9a48f6eabbb0086441121
Ky Bui Van <ky.buivan.dev@gmail.com>
2023-03-30 18:29:22 +0700

    Add libfacedetection

---
+++ external/libfacedetection.cmake
@@ -0,0 +1,8 @@
+# Face detecting library.
+FetchContent_Declare(
+	libfacedetection
+	GIT_REPOSITORY https://github.com/ShiqiYu/libfacedetection.git
+	GIT_TAG v3.0
+)
+
+FetchContent_MakeAvailable(libfacedetection)
