04fb91bf52e9c3ff101efc286b0f5670c16311dc
Ky Bui Van <ky.buivan.dev@gmail.com>
2023-05-11 11:05:03 +0700

    Update build static openal, sdl

--- external/sdl.cmake
+++ external/sdl.cmake
@@ -1,3 +1,6 @@
+set( SDL_SHARED OFF CACHE BOOL "SDL2 option" FORCE )
+set( SDL_STATIC ON CACHE BOOL "SDL2 option" FORCE )
+
 FetchContent_Declare(
     sdl2
     GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
