18e809c706ef5f7b6a61cf0df303f0dd0b655675
Ky Bui Van <ky.buivan.dev@gmail.com>
2023-05-29 13:42:21 +0700

    Update imgui

--- external/imgui.cmake
+++ external/imgui.cmake
@@ -12,6 +12,14 @@
 # copies or substantial portions of the Software.
 #
 
+if(TARGET imgui::imgui)
+    return()
+endif()
+
+message(STATUS "Third-party (external): creating target 'imgui::imgui'")
+
+include(FetchContent)
+
 FetchContent_Declare(
     imgui
     GIT_REPOSITORY https://github.com/ocornut/imgui.git
@@ -19,32 +27,28 @@ FetchContent_Declare(
     GIT_SHALLOW TRUE
 )
 
-FetchContent_GetProperties(imgui)
-if(NOT imgui_POPULATED)
-    FetchContent_Populate(imgui)
-    add_library(imgui STATIC
-        "${imgui_SOURCE_DIR}/imconfig.h"
-        "${imgui_SOURCE_DIR}/imgui.h"
-        "${imgui_SOURCE_DIR}/imgui.cpp"
-        "${imgui_SOURCE_DIR}/imgui_demo.cpp"
-        "${imgui_SOURCE_DIR}/imgui_draw.cpp"
-        "${imgui_SOURCE_DIR}/imgui_internal.h"
-        "${imgui_SOURCE_DIR}/imgui_tables.cpp"
-        "${imgui_SOURCE_DIR}/imgui_widgets.cpp"
-        "${imgui_SOURCE_DIR}/imstb_rectpack.h"
-        "${imgui_SOURCE_DIR}/imstb_textedit.h"
-        "${imgui_SOURCE_DIR}/imstb_truetype.h"
-        "${imgui_SOURCE_DIR}/misc/cpp/imgui_stdlib.h"
-        "${imgui_SOURCE_DIR}/misc/cpp/imgui_stdlib.cpp"
-        "${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.h"
-        "${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp"
-        "${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h"
-        "${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp"
-    )
+FetchContent_MakeAvailable(imgui)
 
-    find_package(OpenGL REQUIRED)
+file(GLOB IMGUI_HEADERS 
+    "${imgui_SOURCE_DIR}/*.h"
+    "${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h"
+    "${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.h"
+    "${imgui_SOURCE_DIR}/misc/cpp/imgui_stdlibs.h"
+)
 
-    target_link_libraries(imgui PUBLIC glfw OpenGL::GL)
-    target_include_directories(imgui PUBLIC "${imgui_SOURCE_DIR}" "${imgui_SOURCE_DIR}/backends")
-    add_library(imgui::imgui ALIAS imgui)
-endif()
+file(GLOB IMGUI_SRC 
+    "${imgui_SOURCE_DIR}/*.cpp"
+    "${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp"
+    "${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp"
+    "${imgui_SOURCE_DIR}/misc/cpp/imgui_stdlib.cpp"
+)
+
+add_library(imgui STATIC ${IMGUI_HEADERS} ${IMGUI_SRC})
+target_include_directories(imgui PUBLIC
+    ${imgui_SOURCE_DIR}
+    ${imgui_SOURCE_DIR}/backends
+    ${imgui_SOURCE_DIR}/misc/cpp
+)
+target_link_libraries(imgui PUBLIC glfw::glfw)
+
+add_library(imgui::imgui ALIAS imgui)
