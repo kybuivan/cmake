0156c7d27997f278b4f5a415d1d9f27a60fd8ecc
Ky Bui Van <ky.buivan.dev@gmail.com>
2023-05-29 14:00:46 +0700

    Update nativefiledialog

--- external/nfd.cmake
+++ external/nfd.cmake
@@ -12,23 +12,20 @@
 # copies or substantial portions of the Software.
 #
 
+if(TARGET nfd::nfd)
+    return()
+endif()
+
+message(STATUS "Third-party (external): creating target 'nfd::nfd'")
+
+include(FetchContent)
+
 FetchContent_Declare(
     nfd
-    GIT_REPOSITORY https://github.com/mlabbe/nativefiledialog.git
-    GIT_TAG        release_116
+    GIT_REPOSITORY https://github.com/btzy/nativefiledialog-extended.git
+    GIT_TAG        v1.0.3
 )
 
-FetchContent_GetProperties(nfd)
-if(NOT nfd_POPULATED)
-    FetchContent_Populate(nfd)
-    add_library(nfd STATIC EXCLUDE_FROM_ALL)
-    target_sources(nfd PRIVATE
-        "${nfd_SOURCE_DIR}/src/nfd_common.c"
-        "$<$<PLATFORM_ID:Linux>:${nfd_SOURCE_DIR}/src/nfd_zenity.c>"
-        "$<$<PLATFORM_ID:Darwin>:${nfd_SOURCE_DIR}/src/nfd_cocoa.m>"
-        "$<$<PLATFORM_ID:Windows>:${nfd_SOURCE_DIR}/src/nfd_win.cpp>"
-    )
-    target_include_directories(nfd PUBLIC "${nfd_SOURCE_DIR}/src/include")
-    target_compile_definitions(nfd PRIVATE _CRT_SECURE_NO_WARNINGS=1)
-    target_compile_options(nfd PRIVATE $<$<C_COMPILER_ID:GNU>:-Wno-format-truncation>)
-endif()
+FetchContent_MakeAvailable(nfd)
+
+add_library(nfd::nfd ALIAS nfd)
