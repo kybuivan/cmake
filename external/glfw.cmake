ff3c793228a4f2db0f3f573b46bb7a3961fc7d6f
Ky Bui Van <ky.buivan.dev@gmail.com>
2023-05-29 13:03:10 +0700

    Update glfw

--- external/glfw.cmake
+++ external/glfw.cmake
@@ -12,15 +12,26 @@
 # copies or substantial portions of the Software.
 #
 
-# set all glfw dependencies
-set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
-set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+if(TARGET glfw::glfw)
+    return()
+endif()
+
+message(STATUS "Third-party (external): creating target 'glfw::glfw'")
+
+include(FetchContent)
 
 FetchContent_Declare(
     glfw
     GIT_REPOSITORY https://github.com/glfw/glfw.git
-    GIT_TAG 3.3.7
+    GIT_TAG 3.3.8
 )
 
+set(GLFW_BUILD_EXAMPLES OFF CACHE INTERNAL "Build the GLFW example programs")
+set(GLFW_BUILD_TESTS OFF CACHE INTERNAL "Build the GLFW test programs")
+set(GLFW_BUILD_DOCS OFF CACHE INTERNAL "Build the GLFW documentation")
+set(GLFW_INSTALL OFF CACHE INTERNAL "Generate installation target")
+set(GLFW_VULKAN_STATIC OFF CACHE INTERNAL "Use the Vulkan loader statically linked into application")
+
 FetchContent_MakeAvailable(glfw)
+
+add_library(glfw::glfw ALIAS glfw)
