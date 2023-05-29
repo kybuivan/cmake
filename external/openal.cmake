fa30e84367f345cbe21ef06065a1a8c1fbef1b13
Ky Bui Van <ky.buivan.dev@gmail.com>
2023-05-29 15:48:06 +0700

    Update OpenAL

--- external/openal.cmake
+++ external/openal.cmake
@@ -12,21 +12,30 @@
 # copies or substantial portions of the Software.
 # 
 
-#add OpenAL library
-set(ALSOFT_EXAMPLES         OFF CACHE BOOL "" FORCE)
-set(ALSOFT_INSTALL_EXAMPLES OFF CACHE BOOL "" FORCE)
-set(ALSOFT_NO_CONFIG_UTIL   OFF CACHE BOOL "" FORCE)
-set(ALSOFT_UTILS            OFF CACHE BOOL "" FORCE)
-set(ALSOFT_INSTALL_UTILS    OFF CACHE BOOL "" FORCE)
-set(ALSOFT_REQUIRE_OPENSL   OFF CACHE BOOL "" FORCE)
-set(ALSOFT_REQUIRE_OBOE     OFF CACHE BOOL "" FORCE)
-set(ALSOFT_REQUIRE_SDL2     OFF CACHE BOOL "" FORCE)
-set(ALSOFT_UPDATE_BUILD_VERSION OFF CACHE BOOL "" FORCE)
-set(LIBTYPE "STATIC")
+if(TARGET OpenAL::OpenAL)
+    return()
+endif()
+
+message(STATUS "Third-party (external): creating target 'OpenAL::OpenAL'")
+
+include(FetchContent)
+ 
 
 FetchContent_Declare(
-    openal_soft
+    OpenAL
     GIT_REPOSITORY https://github.com/kcat/openal-soft.git
-    GIT_TAG 1.22.0
+    GIT_TAG 1.23.1
 )
-FetchContent_MakeAvailable(openal_soft)
+
+#add OpenAL library
+set(ALSOFT_EXAMPLES OFF CACHE BOOL "Build example programs" FORCE)
+set(ALSOFT_INSTALL_EXAMPLES OFF CACHE BOOL "Install example programs (alplay, alstream, ...)" FORCE)
+set(ALSOFT_UTILS OFF CACHE BOOL "Build utility programs" FORCE)
+set(ALSOFT_INSTALL_UTILS OFF CACHE BOOL "Install utility programs (openal-info, alsoft-config, ...)" FORCE)
+set(ALSOFT_REQUIRE_OPENSL OFF CACHE BOOL "Require OpenSL backend" FORCE)
+set(ALSOFT_REQUIRE_OBOE OFF CACHE BOOL "Require Oboe backend" FORCE)
+set(ALSOFT_REQUIRE_SDL2 OFF CACHE BOOL "Require SDL2 backend" FORCE)
+set(ALSOFT_UPDATE_BUILD_VERSION OFF CACHE BOOL "Update git build version info" FORCE)
+set(LIBTYPE "STATIC")
+
+FetchContent_MakeAvailable(OpenAL)
